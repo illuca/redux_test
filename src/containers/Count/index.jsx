208f5d445a0a12010388cba796a6d0e2a52de882
Yuan Long <longyuan@magiccreative.tech>
2022-04-10 16:29:38 +0800

    使用react-redux--少写两行优化容器向UI传参

--- src/containers/Count/index.jsx
+++ src/containers/Count/index.jsx
@@ -4,30 +4,18 @@ import {createIncreaseAction, createIncreaseAsyncAction, createDecreaseAction} f
 
 const {connect} = require("react-redux");
 
-function mapStateToProps() {
-    return {
-        count: store.getState()
-    }
-}
 
-function mapDispatchToProps() {
-    return {
-        increase: (number) => {
-            return store.dispatch(createIncreaseAction(number))
-        },
-        decrease: number => {
-            return store.dispatch(createDecreaseAction(number))
-        },
-        increaseIfOdd: number => {
-            if(store.getState() % 2==1) {
-                return store.dispatch(createIncreaseAction(number))
-            }
-        },
-        increaseSync: (number, time) => {
-            return store.dispatch(createIncreaseAsyncAction(number, time))
-        }
+const CountContainer = connect(
+    // mapStateToProps
+    (state) => {
+        return {count: state}
+    },
+    // mapDispatchToProps
+    {
+        increase: createIncreaseAction,
+        decrease: createDecreaseAction,
+        increaseIfOdd: createIncreaseAction,
+        increaseSync: createIncreaseAsyncAction
     }
-}
-
-const CountContainer = connect(mapStateToProps, mapDispatchToProps)(CountUI)
+)(CountUI)
 export default CountContainer
