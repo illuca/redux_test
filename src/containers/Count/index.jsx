3bd0ad22fbefdec4b7014401665acc5203459520
Yuan Long <longyuan@magiccreative.tech>
2022-04-10 18:47:23 +0800

    使用react-redux----优化provider少写App向count容器传参store
    之前报错是因为Provider引入错了

--- src/containers/Count/index.jsx
+++ src/containers/Count/index.jsx
@@ -1,9 +1,49 @@
-import CountUI from "../../components/Count";
 import store from "../../redux/store";
 import {createIncreaseAction, createIncreaseAsyncAction, createDecreaseAction} from "../../redux/count_action_creator";
+import React, {Component} from "react";
 
 const {connect} = require("react-redux");
 
+
+
+
+class Count extends Component {
+    increase = () => {
+        const value = this.selectNumber.value * 1
+        this.props.increase(value)
+    }
+    decrease = () => {
+        const value = this.selectNumber.value * 1
+        this.props.decrease(value)
+    }
+    increaseIfOdd = () => {
+        const value = this.selectNumber.value * 1
+        if (this.props.count % 2 == 1) {
+            this.props.increase(value)
+        }
+    }
+    increaseSync = () => {
+        const value = this.selectNumber.value * 1
+        this.props.increaseSync(value, 1000)
+    }
+
+    render() {
+        return (
+            <div>
+                <p>当前求和为:{store.getState()}</p>
+                <select ref={(elem) => this.selectNumber = elem}>
+                    <option value={"1"}>1</option>
+                    <option value={"2"}>2</option>
+                    <option value={"3"}>3</option>
+                </select>
+                <button onClick={this.increase}>increase</button>
+                <button onClick={this.decrease}>decrease</button>
+                <button onClick={this.increaseIfOdd}>increaseIfOdd</button>
+                <button onClick={this.increaseSync}>increaseSync</button>
+            </div>
+        );
+    }
+}
 
 const CountContainer = connect(
     // mapStateToProps
@@ -16,5 +56,6 @@ const CountContainer = connect(
         decrease: createDecreaseAction,
         increaseSync: createIncreaseAsyncAction
     }
-)(CountUI)
+)(Count)
 export default CountContainer
+
