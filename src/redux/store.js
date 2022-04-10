db27626dbcd1e7ccc0e93fcb32af0c10a5b73d74
Yuan Long <longyuan@magiccreative.tech>
2022-04-11 00:04:08 +0800

    count和person组件共享数据

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,11 @@ import {createStore, applyMiddleware, combineReducers} from "redux"
 import countReducer from "./reducer/count"
 import personReducer from "./reducer/person";
 import thunk from "redux-thunk";
+import {composeWithDevTools} from "redux-devtools-extension";
 
 const allReducers = combineReducers({
     totalCount: countReducer,
     persons : personReducer
 })
-const store =  createStore(allReducers, applyMiddleware(thunk))
+const store =  createStore(allReducers,composeWithDevTools(applyMiddleware(thunk)))
 export default store;
