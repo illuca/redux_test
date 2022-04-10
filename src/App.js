4984a54291540a9384533fb8c656435398883806
Yuan Long <longyuan@magiccreative.tech>
2022-04-10 16:34:06 +0800

    使用react-redux--少写两行优化容器向UI传参

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import Count from "./components/Count";
-import store from "./redux/store";
 import CountContainer from "./containers/Count";
 
 function App() {
   return (
     <div className="App">
-      <CountContainer store={store}/>
+      <CountContainer/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import store from "./redux/store";
+import Provider from "react-redux/lib/components/Provider";
 
 ReactDOM.render(
-    <App/>,
-    document.getElementById('root')
-);
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+document.getElementById('root')
+)
+;
 
 store.subscribe(() => {
     ReactDOM.render(<App/>, document.getElementById('root'))
